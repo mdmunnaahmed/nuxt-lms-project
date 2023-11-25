085aed76aaaccdfa326203bc0c5d6fc8a0d72e64
mdmunnaahmed <msdmunna77@gmail.com>
2023-11-25 21:36:37 +0600

    shop page --running

---
+++ pages/shop/index.vue
@@ -0,0 +1,467 @@
+<template>
+  <div>
+    <SectionInnerBanner title="Shop wishes" slug="shop" />
+    <!-- Start product Area -->
+    <section id="product_area" class="section-padding">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="section-title">
+              <h2>Our Products</h2>
+              <p>
+                Check Our <span><u>All Products</u></span>
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div class="text-center">
+          <div class="product_filter">
+            <ul>
+              <li class="active filter" data-filter="all">All</li>
+              <li class="filter" data-filter=".sale">Sale</li>
+              <li class="filter" data-filter=".bslr">Bestseller</li>
+              <li class="filter" data-filter=".ftrd">Featured</li>
+            </ul>
+          </div>
+
+          <div class="product_item">
+            <div class="row">
+              <div class="col-lg-3 col-md-4 col-sm-6 mix sale">
+                <div class="product-grid">
+                  <div class="product-image">
+                    <a href="#">
+                      <img
+                        class="pic-1"
+                        src="/images/shop/1.jpg"
+                        alt="product image"
+                      />
+                      <img
+                        class="pic-2"
+                        src="/images/shop/2.jpg"
+                        alt="product image"
+                      />
+                    </a>
+                    <ul class="social">
+                      <li>
+                        <a href="#" data-tip="Quick View"
+                          ><i class="ti-zoom-in"></i
+                        ></a>
+                      </li>
+                      <li>
+                        <a href="#" data-tip="Add to Wishlist"
+                          ><i class="ti-bag"></i
+                        ></a>
+                      </li>
+                      <li>
+                        <a href="#" data-tip="Add to Cart"
+                          ><i class="ti-shopping-cart"></i
+                        ></a>
+                      </li>
+                    </ul>
+                    <span class="product-new-label">Sale</span>
+                  </div>
+                  <ul class="rating">
+                    <li class="fa fa-star"></li>
+                    <li class="fa fa-star"></li>
+                    <li class="fa fa-star"></li>
+                    <li class="fa fa-star"></li>
+                    <li class="fa fa-star"></li>
+                  </ul>
+                  <div class="product-content">
+                    <h3 class="title"><a href="#">Product Title</a></h3>
+                    <div class="price">
+                      $16.00
+                      <span>$20.00</span>
+                    </div>
+                    <a class="add-to-cart" href="#">+ Add To Cart</a>
+                  </div>
+                </div>
+              </div>
+              <!-- End Col -->
+
+              <div class="col-lg-3 col-md-4 col-sm-6 mix ftrd">
+                <div class="product-grid">
+                  <div class="product-image">
+                    <a href="#">
+                      <img
+                        class="pic-1"
+                        src="/images/shop/3.jpg"
+                        alt="product image"
+                      />
+                      <img
+                        class="pic-2"
+                        src="/images/shop/4.jpg"
+                        alt="product image"
+                      />
+                    </a>
+                    <ul class="social">
+                      <li>
+                        <a href="#" data-tip="Quick View"
+                          ><i class="ti-zoom-in"></i
+                        ></a>
+                      </li>
+                      <li>
+                        <a href="#" data-tip="Add to Wishlist"
+                          ><i class="ti-bag"></i
+                        ></a>
+                      </li>
+                      <li>
+                        <a href="#" data-tip="Add to Cart"
+                          ><i class="ti-shopping-cart"></i
+                        ></a>
+                      </li>
+                    </ul>
+                    <span class="product-new-label">-20%</span>
+                  </div>
+                  <ul class="rating">
+                    <li class="fa fa-star"></li>
+                    <li class="fa fa-star"></li>
+                    <li class="fa fa-star"></li>
+                    <li class="fa fa-star"></li>
+                    <li class="fa fa-star"></li>
+                  </ul>
+                  <div class="product-content">
+                    <h3 class="title"><a href="#">Product Title</a></h3>
+                    <div class="price">
+                      $16.00
+                      <span>$20.00</span>
+                    </div>
+                    <a class="add-to-cart" href="#">+ Add To Cart</a>
+                  </div>
+                </div>
+              </div>
+              <!-- End Col -->
+
+              <div class="col-lg-3 col-md-4 col-sm-6 mix">
+                <div class="product-grid">
+                  <div class="product-image">
+                    <a href="#">
+                      <img
+                        class="pic-1"
+                        src="/images/shop/5.jpg"
+                        alt="product image"
+                      />
+                      <img
+                        class="pic-2"
+                        src="/images/shop/6.jpg"
+                        alt="product image"
+                      />
+                    </a>
+                    <ul class="social">
+                      <li>
+                        <a href="#" data-tip="Quick View"
+                          ><i class="ti-zoom-in"></i
+                        ></a>
+                      </li>
+                      <li>
+                        <a href="#" data-tip="Add to Wishlist"
+                          ><i class="ti-bag"></i
+                        ></a>
+                      </li>
+                      <li>
+                        <a href="#" data-tip="Add to Cart"
+                          ><i class="ti-shopping-cart"></i
+                        ></a>
+                      </li>
+                    </ul>
+                    <span class="product-new-label">Sale</span>
+                  </div>
+                  <ul class="rating">
+                    <li class="fa fa-star"></li>
+                    <li class="fa fa-star"></li>
+                    <li class="fa fa-star"></li>
+                    <li class="fa fa-star"></li>
+                    <li class="fa fa-star disable"></li>
+                  </ul>
+                  <div class="product-content">
+                    <h3 class="title"><a href="#">Product Title</a></h3>
+                    <div class="price">
+                      $16.00
+                      <span>$20.00</span>
+                    </div>
+                    <a class="add-to-cart" href="#">+ Add To Cart</a>
+                  </div>
+                </div>
+              </div>
+              <!-- End Col -->
+
+              <div class="col-lg-3 col-md-4 col-sm-6 mix sale bslr">
+                <div class="product-grid">
+                  <div class="product-image">
+                    <a href="#">
+                      <img
+                        class="pic-1"
+                        src="/images/shop/7.jpg"
+                        alt="product image"
+                      />
+                      <img
+                        class="pic-2"
+                        src="/images/shop/8.jpg"
+                        alt="product image"
+                      />
+                    </a>
+                    <ul class="social">
+                      <li>
+                        <a href="#" data-tip="Quick View"
+                          ><i class="ti-zoom-in"></i
+                        ></a>
+                      </li>
+                      <li>
+                        <a href="#" data-tip="Add to Wishlist"
+                          ><i class="ti-bag"></i
+                        ></a>
+                      </li>
+                      <li>
+                        <a href="#" data-tip="Add to Cart"
+                          ><i class="ti-shopping-cart"></i
+                        ></a>
+                      </li>
+                    </ul>
+                    <span class="product-new-label">New</span>
+                  </div>
+                  <ul class="rating">
+                    <li class="fa fa-star"></li>
+                    <li class="fa fa-star"></li>
+                    <li class="fa fa-star"></li>
+                    <li class="fa fa-star"></li>
+                    <li class="fa fa-star"></li>
+                  </ul>
+                  <div class="product-content">
+                    <h3 class="title"><a href="#">Product Title</a></h3>
+                    <div class="price">
+                      $16.00
+                      <span>$20.00</span>
+                    </div>
+                    <a class="add-to-cart" href="#">+ Add To Cart</a>
+                  </div>
+                </div>
+              </div>
+              <!-- End Col -->
+
+              <div class="col-lg-3 col-md-4 col-sm-6 mix ftrd">
+                <div class="product-grid">
+                  <div class="product-image">
+                    <a href="#">
+                      <img
+                        class="pic-1"
+                        src="/images/shop/5.jpg"
+                        alt="product image"
+                      />
+                      <img
+                        class="pic-2"
+                        src="/images/shop/6.jpg"
+                        alt="product image"
+                      />
+                    </a>
+                    <ul class="social">
+                      <li>
+                        <a href="#" data-tip="Quick View"
+                          ><i class="ti-zoom-in"></i
+                        ></a>
+                      </li>
+                      <li>
+                        <a href="#" data-tip="Add to Wishlist"
+                          ><i class="ti-bag"></i
+                        ></a>
+                      </li>
+                      <li>
+                        <a href="#" data-tip="Add to Cart"
+                          ><i class="ti-shopping-cart"></i
+                        ></a>
+                      </li>
+                    </ul>
+                    <span class="product-new-label">Sale</span>
+                  </div>
+                  <ul class="rating">
+                    <li class="fa fa-star"></li>
+                    <li class="fa fa-star"></li>
+                    <li class="fa fa-star"></li>
+                    <li class="fa fa-star"></li>
+                    <li class="fa fa-star"></li>
+                  </ul>
+                  <div class="product-content">
+                    <h3 class="title"><a href="#">Product Title</a></h3>
+                    <div class="price">
+                      $16.00
+                      <span>$20.00</span>
+                    </div>
+                    <a class="add-to-cart" href="#">+ Add To Cart</a>
+                  </div>
+                </div>
+              </div>
+              <!-- End Col -->
+
+              <div class="col-lg-3 col-md-4 col-sm-6 mix sale bslr">
+                <div class="product-grid">
+                  <div class="product-image">
+                    <a href="#">
+                      <img
+                        class="pic-1"
+                        src="/images/shop/1.jpg"
+                        alt="product image"
+                      />
+                      <img
+                        class="pic-2"
+                        src="/images/shop/2.jpg"
+                        alt="product image"
+                      />
+                    </a>
+                    <ul class="social">
+                      <li>
+                        <a href="#" data-tip="Quick View"
+                          ><i class="ti-zoom-in"></i
+                        ></a>
+                      </li>
+                      <li>
+                        <a href="#" data-tip="Add to Wishlist"
+                          ><i class="ti-bag"></i
+                        ></a>
+                      </li>
+                      <li>
+                        <a href="#" data-tip="Add to Cart"
+                          ><i class="ti-shopping-cart"></i
+                        ></a>
+                      </li>
+                    </ul>
+                    <span class="product-new-label">-30%</span>
+                  </div>
+                  <ul class="rating">
+                    <li class="fa fa-star"></li>
+                    <li class="fa fa-star"></li>
+                    <li class="fa fa-star"></li>
+                    <li class="fa fa-star"></li>
+                    <li class="fa fa-star"></li>
+                  </ul>
+                  <div class="product-content">
+                    <h3 class="title"><a href="#">Product Title</a></h3>
+                    <div class="price">
+                      $16.00
+                      <span>$20.00</span>
+                    </div>
+                    <a class="add-to-cart" href="#">+ Add To Cart</a>
+                  </div>
+                </div>
+              </div>
+              <!-- End Col -->
+
+              <div class="col-lg-3 col-md-4 col-sm-6 mix sale bslr">
+                <div class="product-grid">
+                  <div class="product-image">
+                    <a href="#">
+                      <img
+                        class="pic-1"
+                        src="/images/shop/7.jpg"
+                        alt="product image"
+                      />
+                      <img
+                        class="pic-2"
+                        src="/images/shop/8.jpg"
+                        alt="product image"
+                      />
+                    </a>
+                    <ul class="social">
+                      <li>
+                        <a href="#" data-tip="Quick View"
+                          ><i class="ti-zoom-in"></i
+                        ></a>
+                      </li>
+                      <li>
+                        <a href="#" data-tip="Add to Wishlist"
+                          ><i class="ti-bag"></i
+                        ></a>
+                      </li>
+                      <li>
+                        <a href="#" data-tip="Add to Cart"
+                          ><i class="ti-shopping-cart"></i
+                        ></a>
+                      </li>
+                    </ul>
+                    <span class="product-new-label">Sale</span>
+                  </div>
+                  <ul class="rating">
+                    <li class="fa fa-star"></li>
+                    <li class="fa fa-star"></li>
+                    <li class="fa fa-star"></li>
+                    <li class="fa fa-star"></li>
+                    <li class="fa fa-star"></li>
+                  </ul>
+                  <div class="product-content">
+                    <h3 class="title"><a href="#">Product Title</a></h3>
+                    <div class="price">
+                      $16.00
+                      <span>$20.00</span>
+                    </div>
+                    <a class="add-to-cart" href="#">+ Add To Cart</a>
+                  </div>
+                </div>
+              </div>
+              <!-- End Col -->
+
+              <div class="col-lg-3 col-md-4 col-sm-6 mix sale bslr">
+                <div class="product-grid">
+                  <div class="product-image">
+                    <a href="#">
+                      <img
+                        class="pic-1"
+                        src="/images/shop/3.jpg"
+                        alt="product image"
+                      />
+                      <img
+                        class="pic-2"
+                        src="/images/shop/4.jpg"
+                        alt="product image"
+                      />
+                    </a>
+                    <ul class="social">
+                      <li>
+                        <a href="#" data-tip="Quick View"
+                          ><i class="ti-zoom-in"></i
+                        ></a>
+                      </li>
+                      <li>
+                        <a href="#" data-tip="Add to Wishlist"
+                          ><i class="ti-bag"></i
+                        ></a>
+                      </li>
+                      <li>
+                        <a href="#" data-tip="Add to Cart"
+                          ><i class="ti-shopping-cart"></i
+                        ></a>
+                      </li>
+                    </ul>
+                    <span class="product-new-label">-50%</span>
+                  </div>
+                  <ul class="rating">
+                    <li class="fa fa-star"></li>
+                    <li class="fa fa-star"></li>
+                    <li class="fa fa-star"></li>
+                    <li class="fa fa-star"></li>
+                    <li class="fa fa-star"></li>
+                  </ul>
+                  <div class="product-content">
+                    <h3 class="title"><a href="#">Product Title</a></h3>
+                    <div class="price">
+                      $16.00
+                      <span>$20.00</span>
+                    </div>
+                    <a class="add-to-cart" href="#">+ Add To Cart</a>
+                  </div>
+                </div>
+              </div>
+              <!-- End Col -->
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- End product Area -->
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+</style>
